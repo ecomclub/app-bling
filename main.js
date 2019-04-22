9b5d85c7339e18988f70cb4129af91339bd3b60e
Talisson Ferreira <talissonf@gmail.com>
2019-04-22 17:25:59 -0300

    test: debug .envs

--- main.js
+++ main.js
@@ -4,8 +4,12 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const logger = require('console-files')
+
+logger.log(process.env)
 
 require('./bin/uncaughtException')
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(require('./lib/routes'))
