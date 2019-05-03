3011b97bb63687a72efba3be9e98bf20c5058a51
Talisson Ferreira <talissonf@gmail.com>
2019-05-02 23:17:13 -0300

    perf: loads application dependencies to inject wherever required

--- main.js
+++ main.js
@@ -2,18 +2,21 @@
 
 const bodyParser = require('body-parser')
 const express = require('express')
+const sqlite = require('sqlite3').verbose()
 const app = express()
-const port = process.env.PORT || 3000
-const logger = require('console-files')
+const port = process.env.PORT || 4200
 
-logger.log(process.env)
-
+//
+const { ecomAuth } = require('ecomplus-app-sdk')
+const Bling = require('bling-erp-sdk')
+const mysql = require('./lib/database')
+const db = new sqlite.Database(process.env.ECOM_AUTH_DB)
+//
 require('./bin/uncaughtException')
+require('./lib/services/setup-stores')({ db, ecomAuth })
+require('./lib/services/synchronizations')({ db, ecomAuth, mysql, Bling })
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(require('./lib/routes'))
+app.use(require('./lib/routes')({ ecomAuth, mysql, Bling, db }))
 app.listen(port)
-
-//
-require('./lib/app-manager')
