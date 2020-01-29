240619267c11690a02ea47e1d6a8cf29aec65282
Talisson Ferreira <talissonf@gmail.com>
2020-01-29 15:21:10 -0300

    chore: update mysql dump

--- assets/bling.sql
+++ assets/bling.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 29, 2020 at 02:22 PM
+-- Generation Time: Jan 29, 2020 at 03:13 PM
 -- Server version: 10.1.43-MariaDB-0ubuntu0.18.04.1
 -- PHP Version: 7.2.24-0ubuntu0.18.04.2
 
@@ -32,7 +32,7 @@ CREATE TABLE `ecomplus_orders` (
   `updated_at` datetime DEFAULT NULL,
   `order_store_id` int(10) NOT NULL,
   `order_ecom_id` varchar(24) DEFAULT NULL,
-  `order_loja_id` int(10) NOT NULL,
+  `order_loja_id` int(10) DEFAULT NULL,
   `order_ecom_status` varchar(255) DEFAULT NULL,
   `order_bling_status` varchar(255) DEFAULT NULL,
   `order_bling_id` int(11) DEFAULT NULL,
@@ -56,7 +56,7 @@ CREATE TABLE `ecomplus_products` (
   `product_bling_price` decimal(10,0) NOT NULL,
   `product_bling_stock` int(11) DEFAULT NULL,
   `product_store_id` int(11) NOT NULL,
-  `product_loja_id` int(10) NOT NULL,
+  `product_loja_id` int(10) DEFAULT NULL,
   `product_id` varchar(24) NOT NULL,
   `error` tinyint(255) NOT NULL DEFAULT '0',
   `product_bling_id` bigint(255) DEFAULT NULL
@@ -82,20 +82,6 @@ CREATE TABLE `ecomplus_products_variations` (
   `variation_bling_id` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `transaction_history`
---
-
-CREATE TABLE `transaction_history` (
-  `id` int(11) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` datetime DEFAULT NULL,
-  `type` enum('order','product','stock') NOT NULL,
-  `stock` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 --
 -- Indexes for dumped tables
 --
@@ -119,12 +105,6 @@ ALTER TABLE `ecomplus_products_variations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `transaction_history`
---
-ALTER TABLE `transaction_history`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -132,22 +112,17 @@ ALTER TABLE `transaction_history`
 -- AUTO_INCREMENT for table `ecomplus_orders`
 --
 ALTER TABLE `ecomplus_orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `ecomplus_products`
 --
 ALTER TABLE `ecomplus_products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
 --
 -- AUTO_INCREMENT for table `ecomplus_products_variations`
 --
 ALTER TABLE `ecomplus_products_variations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
---
--- AUTO_INCREMENT for table `transaction_history`
---
-ALTER TABLE `transaction_history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
