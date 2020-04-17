0513c4d0853552983276cd5360c01178cc4ace16
Talisson Ferreira <talissonf@gmail.com>
2020-04-17 12:50:48 -0300

    feat(webhook): queue to prevent processing the same body several times

--- routes/ecom/webhook.js
+++ routes/ecom/webhook.js
@@ -14,6 +14,7 @@ const SKIP_TRIGGER_NAME = 'SkipTrigger'
 const ECHO_SUCCESS = 'SUCCESS'
 const ECHO_SKIP = 'SKIP'
 const ECHO_API_ERROR = 'STORE_API_ERR'
+let processQueue = []
 
 module.exports = (appSdk) => {
   return (req, res) => {
@@ -25,120 +26,158 @@ module.exports = (appSdk) => {
     const trigger = req.body
     const orderId = trigger.resource_id
 
-    // get app configured options
-    getConfig({ appSdk, storeId }, true)
+    if (processQueue.indexOf(orderId) !== -1) {
+      return res.send(ECHO_SKIP)
+    }
 
-      .then(configObj => {
-        /* Do the stuff */
-        if (!configObj.bling_api_key) {
-          const err = new Error('Token unset')
-          err.name = SKIP_TRIGGER_NAME
-          throw err
-        }
-        return configObj
-      })
+    processQueue.push(orderId)
 
-      .then(configObj => {
-        return new Promise((resolve, reject) => {
-          const query = 'SELECT * FROM ecomplus_orders WHERE order_ecom_id = ? AND order_store_id = ?'
-          return database
-            .pool
-            .query(query, [orderId, storeId], (error, row) => {
-              if (error) {
-                const err = error
-                err.name = SKIP_TRIGGER_NAME
-                reject(err)
-              } else if (!row || !row[0]) {
-                const err = new Error('Order not found')
-                err.name = SKIP_TRIGGER_NAME
-                reject(err)
-              } else if (row[0]) {
-                resolve({ configObj })
-              }
-            })
+    setTimeout(() => {
+      // get app configured options
+      getConfig({ appSdk, storeId }, true)
+
+        .then(configObj => {
+          /* Do the stuff */
+          if (!configObj.bling_api_key) {
+            const err = new Error('Token unset')
+            err.name = SKIP_TRIGGER_NAME
+            throw err
+          }
+          return configObj
         })
-      })
 
-      .then(({ configObj }) => {
-        const url = `/orders/${orderId}.json`
-        return appSdk
-          .apiRequest(storeId, url)
-          .then(resp => ({ data: resp.response.data, configObj }))
-      })
-
-      .then(({ data, configObj }) => {
-        const blingClient = new Bling({ apiKey: configObj.bling_api_key })
-        if (data.financial_status) {
-          return blingClient
-            .pedidos
-            .getById(data.number)
-            .then(resp => {
-              let response
-              try {
-                response = JSON.parse(resp)
-              } catch (error) {
-                console.error('json parse error')
-              }
-
-              const { erros, pedidos } = response.retorno
-              if (!erros && Array.isArray(pedidos) && pedidos[0] && pedidos[0].pedido) {
-                const { pedido } = pedidos[0]
-                const { current } = data.financial_status
-                switch (current) {
-                  case 'voided':
-                    if (pedido.situacao.toLowerCase() === 'atendido' ||
-                      pedido.situacao.toLowerCase() === 'venda agenciada' ||
-                      pedido.situacao.toLowerCase() === 'em aberto') {
-                      // cancela
-                      return blingClient
-                        .pedidos
-                        .update(data.number, {
-                          pedido: {
-                            idSituacao: 12
-                          }
-                        })
-                        .then(() => logger.log(`_ order ${data.number} status changed to cancelado | bling / #${storeId}`))
-                    }
-                    break
-                  case 'paid':
-                    // se tiver cancelado
-                    // atualiza para atendido
-                    if (pedido.situacao === 'Cancelado') {
-                      // cancela
-                      return blingClient
-                        .pedidos
-                        .update(data.number, {
-                          pedido: {
-                            idSituacao: 9
-                          }
-                        })
-                        .then(() => logger.log(`_ order ${data.number} status changed to atendido | bling / #${storeId}`))
-                    }
-                    break
-                  default: break
+        .then(configObj => {
+          return new Promise((resolve, reject) => {
+            const query = 'SELECT * FROM ecomplus_orders WHERE order_ecom_id = ? AND order_store_id = ?'
+            return database
+              .pool
+              .query(query, [orderId, storeId], (error, row) => {
+                if (error) {
+                  const err = error
+                  err.name = SKIP_TRIGGER_NAME
+                  reject(err)
+                } else if (!row || !row[0]) {
+                  const err = new Error('Order not found')
+                  err.name = SKIP_TRIGGER_NAME
+                  reject(err)
+                } else if (row[0]) {
+                  resolve({ configObj })
                 }
-              }
-            })
-        }
-      })
-
-      .then(() => res.send(ECHO_SUCCESS))
-
-      .catch(err => {
-        if (err.name === SKIP_TRIGGER_NAME) {
-          // trigger ignored by app configuration
-          res.send(ECHO_SKIP)
-        } else {
-          // logger.error(err)
-          // request to Store API with error response
-          // return error status code
-          res.status(500)
-          let { message } = err
-          res.send({
-            error: ECHO_API_ERROR,
-            message
+              })
           })
-        }
-      })
+        })
+
+        .then(({ configObj }) => {
+          const url = `/orders/${orderId}.json`
+          return appSdk
+            .apiRequest(storeId, url)
+            .then(resp => ({ data: resp.response.data, configObj }))
+        })
+
+        .then(({ data, configObj }) => {
+          const blingClient = new Bling({ apiKey: configObj.bling_api_key })
+          if (data.financial_status) {
+            return blingClient
+              .pedidos
+              .getById(data.number)
+              .then(resp => {
+                let response
+                try {
+                  response = JSON.parse(resp)
+                } catch (error) {
+                  console.error('json parse error')
+                }
+
+                const { erros, pedidos } = response.retorno
+                if (!erros && Array.isArray(pedidos) && pedidos[0] && pedidos[0].pedido) {
+                  const { pedido } = pedidos[0]
+                  const { fields } = trigger
+
+                  if ((fields && fields.includes('financial_status')) || (fields && fields.includes('fulfillment_status'))) {
+                    // parse status
+                    const { current } = data.financial_status
+                    let blingStatus = parseEcomStatus(current)
+
+                    if (data.fulfillment_status &&
+                      data.fulfillment_status.current &&
+                      data.fulfillment_status.current === 'shipped' &&
+                      current === 'paid'
+                    ) {
+                      blingStatus = 'Atendido'
+                    }
+
+                    if (blingStatus && (blingStatus.toLowerCase() !== pedido.situacao.toLowerCase())) {
+                      blingClient.situacao
+                        .fetch()
+                        .then(resp => {
+                          let response
+                          try {
+                            response = JSON.parse(resp)
+                          } catch (error) {
+                            console.error('json parse error')
+                          }
+
+                          const { situacoes } = response.retorno
+
+                          const situacao = situacoes.find(el => el.situacao.nome.toLowerCase() === blingStatus.toLowerCase())
+
+                          if (situacao) {
+                            const { id } = situacao.situacao
+                            const update = {
+                              pedido: {
+                                idSituacao: id
+                              }
+                            }
+
+                            blingClient.pedidos.update(data.number, update)
+                              .then(() => logger.log(`> changed bling situação / #${storeId}`))
+                          }
+                        })
+                    }
+                  }
+                }
+              })
+          }
+        })
+
+        .then(() => res.send(ECHO_SUCCESS))
+
+        .catch(err => {
+          if (err.name === SKIP_TRIGGER_NAME) {
+            // trigger ignored by app configuration
+            return res.send(ECHO_SKIP)
+          } else {
+            // logger.error(err)
+            // request to Store API with error response
+            // return error status code
+            res.status(500)
+            let { message } = err
+            res.send({
+              error: ECHO_API_ERROR,
+              message
+            })
+          }
+        })
+      processQueue.splice(processQueue.indexOf(orderId), 1)
+    }, Math.random() * (5000 - 1000) + 1000)
+  }
+}
+
+const parseEcomStatus = status => {
+  switch (status) {
+    case 'pending':
+    case 'under_analysis':
+    case 'authorized':
+    case 'unauthorized':
+    case 'partially_paid':
+      return 'Em aberto'
+    case 'paid':
+      return 'Venda Agenciada'
+    case 'in_dispute':
+    case 'partially_refunded':
+    case 'refunded':
+    case 'voided':
+      return 'Cancelado'
+    case 'unknown': break
   }
 }
