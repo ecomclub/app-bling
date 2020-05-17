cdd25865e2d34c38e0b4ee2dbcd37ace077bc1fe
Leonardo Matos <leomp120894@gmail.com>
2020-05-16 23:21:49 -0300

    fix(bling-webhook): fix handling bling order webhooks

--- routes/bling/webhook.js
+++ routes/bling/webhook.js
@@ -1,4 +1,5 @@
 'use strict'
+
 const getConfig = require(process.cwd() + '/lib/store-api/get-config')
 const SKIP_TRIGGER_NAME = 'SkipTrigger'
 const ECHO_SUCCESS = 'SUCCESS'
@@ -6,9 +7,9 @@ const ECHO_SKIP = 'SKIP'
 const ECHO_API_ERROR = 'STORE_API_ERR'
 const logger = require('console-files')
 const Bling = require('bling-erp-sdk')
-// ecomClinet
+const { randomObjectId } = require('@ecomplus/utils')
 const ecomClient = require('@ecomplus/client')
-let processQueue = []
+const processQueue = []
 
 module.exports = (appSdk) => {
   return (req, res) => {
@@ -49,7 +50,7 @@ module.exports = (appSdk) => {
               .getById(trigger.codigo)
 
               .then(data => {
-                let result = JSON.parse(data)
+                const result = JSON.parse(data)
                 const { produtos } = result.retorno
                 if (produtos && Array.isArray(produtos) && produtos[0] && produtos[0].produto) {
                   return produtos[0].produto
@@ -76,7 +77,7 @@ module.exports = (appSdk) => {
                   .then(({ data }) => {
                     const { result } = data
                     if (result.length) {
-                      let product = result.find(product => product.sku === sku)
+                      const product = result.find(product => product.sku === sku)
                       let url = `/products/${product._id}.json`
 
                       return ecomClient
@@ -86,19 +87,19 @@ module.exports = (appSdk) => {
                           if (!produto.codigoPai) {
                             // produto
                             if (data.quantity !== produto.estoqueAtual) {
-                              let body = {
+                              const body = {
                                 quantity: Math.sign(produto.estoqueAtual) === -1 ? 0 : produto.estoqueAtual
                               }
                               promise = appSdk.apiRequest(storeId, url, 'PATCH', body)
                             }
                           } else {
                             // variations
-                            let variation = data.variations.find(variation => variation.sku === produto.codigo)
+                            const variation = data.variations.find(variation => variation.sku === produto.codigo)
                             if (variation) {
                               // variação existe na ecomplus
                               if (variation.quantity !== produto.estoqueAtual) {
                                 url = `/products/${product._id}/variations/${variation._id}.json`
-                                let body = {
+                                const body = {
                                   quantity: Math.sign(produto.estoqueAtual) === -1 ? 0 : produto.estoqueAtual
                                 }
                                 promise = appSdk.apiRequest(storeId, url, 'PATCH', body)
@@ -111,9 +112,14 @@ module.exports = (appSdk) => {
                                 .getById(sku)
 
                                 .then(resp => {
-                                  let result = JSON.parse(resp)
+                                  const result = JSON.parse(resp)
                                   const { produtos } = result.retorno
-                                  if (produtos && Array.isArray(produtos) && produtos[0] && produtos[0].produto) {
+                                  if (
+                                    produtos &&
+                                    Array.isArray(produtos) &&
+                                    produtos[0] &&
+                                    produtos[0].produto
+                                  ) {
                                     return produtos[0].produto
                                   } else {
                                     // todo
@@ -121,11 +127,12 @@ module.exports = (appSdk) => {
                                 })
 
                                 .then(produto => {
-                                  let match = produto.variacoes.find(variacao => variacao.variacao.codigo === trigger.codigo)
+                                  const match = produto.variacoes
+                                    .find(variacao => variacao.variacao.codigo === trigger.codigo)
                                   if (match) {
-                                    let variacaoTipo = match.variacao.nome.split(':')[0]
-                                    let variacaoNome = match.variacao.nome.split(':')[1]
-                                    let body = {
+                                    const variacaoTipo = match.variacao.nome.split(':')[0]
+                                    const variacaoNome = match.variacao.nome.split(':')[1]
+                                    const body = {
                                       name: produto.descricao + ' / ' + variacaoNome,
                                       sku: trigger.codigo,
                                       quantity: match.variacao.estoqueAtual
@@ -143,7 +150,9 @@ module.exports = (appSdk) => {
                                 })
                             }
                           }
-                          return promise.then(() => logger.log(`Estoque do produto ${sku} alterado de ${data.quantity} para ${produto.estoqueAtual}`))
+                          return promise.then(() => {
+                            logger.log(`#${storeId} estoque ${sku}: ${data.quantity} => ${produto.estoqueAtual}`)
+                          })
                         })
                     } else {
                       const ecomplusProductSchema = require('./../../lib/schemas/ecomplus-products')
@@ -152,7 +161,7 @@ module.exports = (appSdk) => {
                         .getById(sku)
 
                         .then(data => {
-                          let result = JSON.parse(data)
+                          const result = JSON.parse(data)
                           const { produtos } = result.retorno
                           if (produtos && Array.isArray(produtos) && produtos[0] && produtos[0].produto) {
                             return produtos[0].produto
@@ -162,8 +171,8 @@ module.exports = (appSdk) => {
                         })
 
                         .then(produto => {
-                          let body = ecomplusProductSchema(produto)
-                          let resource = `/products.json`
+                          const body = ecomplusProductSchema(produto)
+                          let resource = '/products.json'
                           return appSdk
                             .apiRequest(storeId, resource, 'POST', body)
 
@@ -171,9 +180,9 @@ module.exports = (appSdk) => {
                               if (!produto.codigoPai && produto.variacoes.length) {
                                 resource = `/products/${resp.response.data._id}/variations.json`
                                 produto.variacoes.forEach(variacao => {
-                                  let variacaoTipo = variacao.variacao.nome.split(':')[0]
-                                  let variacaoNome = variacao.variacao.nome.split(':')[1]
-                                  let body = {
+                                  const variacaoTipo = variacao.variacao.nome.split(':')[0]
+                                  const variacaoNome = variacao.variacao.nome.split(':')[1]
+                                  const body = {
                                     name: produto.descricao + ' / ' + variacaoNome,
                                     sku: variacao.variacao.codigo,
                                     quantity: variacao.variacao.estoqueAtual
@@ -196,100 +205,113 @@ module.exports = (appSdk) => {
             const { sync } = configObj
             // update orders
             const trigger = retorno.pedidos[0].pedido
-            let resource = `/orders.json?number=${trigger.numero}&fields=_id,number,status,financial_status,fulfillment_status,shipping_lines,buyers,items`
-            let method = 'GET'
+            let resource = `/orders.json?number=${trigger.numero}` +
+              '&fields=_id,number,status,financial_status,fulfillment_status,shipping_lines' +
+              ',buyers,items,hidden_metafields'
+            const method = 'GET'
 
-            if (processQueue.indexOf(trigger.numero) === -1) {
+            if (processQueue.indexOf(trigger.numero) === -1 && sync && sync.bling) {
               setTimeout(() => {
                 appSdk
                   .apiRequest(storeId, resource, method)
                   .then(resp => resp.response.data.result)
                   .then(result => {
-                    let promises = []
-                    let order = result[0]
+                    const promises = []
+                    const order = result[0]
 
-                    // invoices?
-                    if (trigger.nota &&
-                      sync &&
-                      sync.bling &&
-                      sync.bling.invoices &&
-                      sync.bling.invoices === true &&
-                      (typeof order === 'object')
-                    ) {
-                      const promise = new Promise(resolve => {
-                        // verifica se a nota ja existe na order
-                        const shippingInvoices = order.shipping_lines.find(shippin => shippin.invoices)
-                        let match
-                        if (shippingInvoices) {
-                          match = shippingInvoices.invoices.find(invoice => invoice.number === trigger.nota.chaveAcesso)
-                        }
+                    if (order) {
+                      if (trigger.nota && sync.bling.invoices) {
+                        const promise = new Promise(resolve => {
+                          // verifica se a nota ja existe na order
+                          const shippingInvoices = order.shipping_lines.find(shippin => shippin.invoices)
+                          let match
+                          if (shippingInvoices) {
+                            match = shippingInvoices.invoices
+                              .find(invoice => invoice.number === trigger.nota.chaveAcesso)
+                          }
 
-                        if (!shippingInvoices || !match) {
-                          const update = [
-                            {
-                              number: data.nota.chaveAcesso,
-                              serial_number: data.nota.numero,
-                              access_key: data.nota.chaveAcesso
-                            }
-                          ]
-                          resource = `/orders/${order._id}/shipping_lines/${order.shipping_lines[0]._id}.json`
-                          appSdk.apiRequest(storeId, resource, 'PATCH', { invoices: update })
-                        }
-                        resolve()
-                      })
-                      promises.push(promise)
-                    }
+                          if (!shippingInvoices || !match) {
+                            const update = [
+                              {
+                                number: data.nota.chaveAcesso,
+                                serial_number: data.nota.numero,
+                                access_key: data.nota.chaveAcesso
+                              }
+                            ]
+                            resource = `/orders/${order._id}/shipping_lines/${order.shipping_lines[0]._id}.json`
+                            appSdk.apiRequest(storeId, resource, 'PATCH', { invoices: update })
+                          }
+                          resolve()
+                        })
+                        promises.push(promise)
+                      }
 
-                    // update financial_status
-                    if (sync &&
-                      sync.bling &&
-                      sync.bling.financial_status &&
-                      sync.bling.financial_status === true
-                    ) {
-                      if ((order && !order.financial_status) || (order.financial_status.current !== parseStatus(trigger.situacao.toLowerCase()))) {
-                        if (parseStatus(trigger.situacao) !== 'unknown') {
-                          const update = {
+                      if (sync.bling.financial_status) {
+                        const blingStatus = trigger.situacao.toLowerCase()
+                        const current = parseStatus(blingStatus)
+                        if (
+                          current &&
+                          (!order.financial_status || order.financial_status.current !== current)
+                        ) {
+                          const url = `/orders/${order._id}.json`
+                          const updatedAt = new Date().toISOString()
+                          const data = {
                             financial_status: {
-                              current: parseStatus(trigger.situacao)
+                              updated_at: updatedAt,
+                              current
                             }
                           }
-                          const resource = `/orders/${order._id}.json`
-                          const promise = appSdk.apiRequest(storeId, resource, 'PATCH', update)
-                          promises.push(promise)
+                          if (
+                            blingStatus === 'atendido' &&
+                            (!order.fulfillment_status || order.fulfillment_status.current !== 'delivered')
+                          ) {
+                            data.fulfillment_status = {
+                              updated_at: updatedAt,
+                              current: 'shipped'
+                            }
+                          }
+                          if (!order.hidden_metafields || order.hidden_metafields.length < 100) {
+                            const metafield = {
+                              _id: randomObjectId(),
+                              namespace: 'bling',
+                              field: 'order-status-sync',
+                              value: `${blingStatus} => ${current} (${updatedAt})`
+                            }
+                            data.hidden_metafields = !order.hidden_metafields ? [metafield]
+                              : order.hidden_metafields.concat([metafield])
+                          }
+                          promises.push(appSdk.apiRequest(storeId, url, 'PATCH', data))
                         }
                       }
-                    }
 
-                    //
-                    if (sync &&
-                      sync.bling &&
-                      sync.bling.shipping_lines &&
-                      sync.bling.shipping_lines === true &&
-                      (typeof order === 'object')
-                    ) {
-                      if (trigger.transporte && trigger.transporte.volumes && order.shipping_lines) {
-                        let codes = []
+                      if (sync.bling.shipping_lines) {
+                        if (trigger.transporte && trigger.transporte.volumes && order.shipping_lines) {
+                          const codes = []
 
-                        trigger.transporte.volumes.forEach(volume => {
-                          if (volume.volume && volume.volume.codigoRastreamento) {
-                            codes.push({
-                              codigo: volume.volume.codigoRastreamento,
-                              tag: volume.volume.servico
-                            })
-                          }
-                        })
-
-                        if (codes.length) {
-                          const updateCodes = []
-                          codes.forEach(code => {
-                            updateCodes.push({
-                              code: code.codigo,
-                              tag: code.tag.replace(' ', '').toLowerCase()
-                            })
+                          trigger.transporte.volumes.forEach(volume => {
+                            if (volume.volume && volume.volume.codigoRastreamento) {
+                              codes.push({
+                                codigo: volume.volume.codigoRastreamento,
+                                tag: volume.volume.servico
+                              })
+                            }
                           })
-                          const resource = `/orders/${order._id}/shipping_lines/${order.shipping_lines[0]._id}.json`
-                          const promise = appSdk.apiRequest(storeId, resource, 'PATCH', { tracking_codes: updateCodes })
-                          promises.push(promise)
+
+                          if (codes.length) {
+                            const updateCodes = []
+                            codes.forEach(code => {
+                              updateCodes.push({
+                                code: code.codigo,
+                                tag: code.tag.replace(' ', '').toLowerCase()
+                              })
+                            })
+                            const resource = `/orders/${order._id}` +
+                              `/shipping_lines/${order.shipping_lines[0]._id}.json`
+                            const promise = appSdk.apiRequest(storeId, resource, 'PATCH', {
+                              tracking_codes: updateCodes
+                            })
+                            promises.push(promise)
+                          }
                         }
                       }
                     }
@@ -297,9 +319,11 @@ module.exports = (appSdk) => {
                     Promise.all(promises).then(() => {
                       logger.log(`Pedido ${trigger.numero} alterado via bling | store #${storeId}`)
                     }).catch(e => {
+                      const err = new Error(`Erro ao atualizar o pedido ${trigger.numero} da loja ${storeId}`)
                       if (e.response && e.response.data) {
-                        logger.error(`Error ao atualizar o pedido ${trigger.numero} da loja ${storeId} | ${JSON.stringify(e.response.data)}`)
+                        err.data = JSON.stringify(e.response.data)
                       }
+                      logger.error(err)
                     })
                   })
                   .catch(e => logger.error('BlingUpdateErr', e))
@@ -323,7 +347,7 @@ module.exports = (appSdk) => {
           // se responder com erro a api para de se comunicar com o app
 
           res.status(200)
-          let { message } = err
+          const { message } = err
           res.send({
             error: ECHO_API_ERROR,
             message
@@ -333,16 +357,17 @@ module.exports = (appSdk) => {
   }
 }
 
-const parseStatus = (status) => {
+const parseStatus = status => {
   switch (status) {
     case 'em aberto':
     case 'em andamento':
     case 'em digitação':
-      return 'pending' // financial_status.current
+      return 'pending'
     case 'venda agenciada':
-    case 'atendido': // financial_status.current  
+    case 'atendido':
       return 'paid'
-    case 'cancelado': // financial_status.current
+    case 'cancelado':
       return 'voided'
   }
+  return null
 }
