d71e136411fb5a8858017714f1f2e754667d249b
Talisson Ferreira <talissonf@gmail.com>
2020-11-06 19:08:45 -0300

    fix(#19): upload product pictures to ecom-storage

--- routes/bling/products.js
+++ routes/bling/products.js
@@ -1,182 +1,88 @@
 'use strict'
 const newProduct = require('./../../lib/store-api/new-product')
 
-module.exports = ({ appSdk, getConfig, logger, blingClient }) => {
-  return (req, res) => {
-    const storeId = parseInt(req.get('x-store-id'), 10) || req.query.storeId
-    const { body } = req
+module.exports = ({ appSdk, getConfig, logger, blingClient }) => async (req, res) => {
+  const storeId = parseInt(req.get('x-store-id'), 10) || req.query.storeId
+  const { body } = req
 
-    if (!storeId || storeId <= 100) {
+  if (!storeId || storeId <= 100) {
+    res.status(401)
+    return res.send({
+      error: 'Unauthorized',
+      message: 'Missing store_id'
+    })
+  }
+
+  if (!Array.isArray(body) || !body.length) {
+    res.status(400)
+    return res.send({
+      error: 'bad format',
+      message: 'Body must be a arrays of sku`s'
+    })
+  }
+
+  const auth = await appSdk.getAuth(storeId)
+
+  const sendToEcom = (produtoSku, apiKey) => blingClient({
+    url: `produto/${produtoSku}`,
+    method: 'get',
+    apiKey
+  }).then(({ data }) => {
+    const { produtos } = data.retorno
+    if (!produtos || !Array.isArray(produtos)) {
+      const err = new Error(`Produto ${produtoSku} não encontrado no Bling`)
+      err.name = 'ItemNotFound'
+      return reject(err)
+    }
+
+    return produtos[0].produto
+  })
+    .then(produto => newProduct(produto, storeId, auth))
+    .then(productBody => {
+      return appSdk.apiRequest(storeId, '/products.json', 'POST', productBody, auth)
+    }).then(({ response }) => {
+      return {
+        produto: produtoSku,
+        _id: response.data._id,
+        error: false
+      }
+    })
+    .catch(e => {
+      return {
+        produto: produtoSku,
+        _id: null,
+        error: true
+      }
+    })
+
+  return getConfig({ appSdk, storeId }, true).then(configObj => {
+    if (!configObj || !configObj.bling_api_key) {
       res.status(401)
       return res.send({
         error: 'Unauthorized',
-        message: 'Missing store_id'
+        message: 'Chave da api não configurada no aplicativo.'
       })
     }
+    return configObj
+  }).then(configObj => {
+    res.end()
+    const apiKey = configObj.bling_api_key
+    const promises = []
+    body.forEach(produtoId => {
+      promises.push(sendToEcom(produtoId, apiKey))
+    })
 
-    if (!Array.isArray(body) || !body.length) {
-      res.status(400)
-      return res.send({
-        error: 'bad format',
-        message: 'Body must be a arrays of sku`s'
+    return Promise.all(promises)
+  }).then((result) => logger.log('Importação Bling', JSON.stringify({ result, storeId }, undefined, 2)))
+    .catch(err => {
+      logger.error('syncerr', err)
+      // logger.error(err)
+      // request to Store API with error response
+      // return error status code
+      res.status(500)
+      const { message } = err
+      res.send({
+        message
       })
-    }
-
-    // get app configured options
-    getConfig({ appSdk, storeId }, true)
-
-      .then(configObj => {
-        if (!configObj || !configObj.bling_api_key) {
-          res.status(401)
-          return res.send({
-            error: 'Unauthorized',
-            message: 'Missing bling_api_key'
-          })
-        }
-        return configObj
-      })
-
-      .then(configObj => {
-        const apiKey = configObj.bling_api_key
-        let current = 0
-        const nextProduct = () => {
-          current++
-          return sync()
-        }
-
-        const sync = () => {
-          if (body[current]) {
-            blingClient({
-              url: `produto/${body[current]}`,
-              method: 'get',
-              apiKey
-            })
-              .then(({ data }) => {
-                const { produtos } = data.retorno
-                if (produtos && Array.isArray(produtos) && produtos[0] && produtos[0].produto) {
-                  return produtos[0].produto
-                } else {
-                  // todo
-                  throw new Error(`Product ${body[current]} not found in bling`)
-                }
-              })
-
-              .then(produto => {
-                const schema = newProduct(produto)
-                return appSdk
-                  .apiRequest(storeId, '/products.json', 'POST', schema)
-                  .then(({ response }) => ({ data: response.data, produto }))
-              })
-
-              .then(({ data, produto }) => {
-                if (!produto.codigoPai && produto.variacoes && produto.variacoes.length) {
-                  const resource = `/products/${data._id}/variations.json`
-                  const { variacoes } = produto
-
-                  variacoes.forEach(variacao => {
-                    const pVariacao = variacao.variacao
-                    const variations = pVariacao.nome.split(';')
-                    const specifications = {}
-
-                    // specifications
-                    for (let i = 0; i < variations.length; i++) {
-                      if (variations[i] !== '') {
-                        const variation = variations[i].split(':')
-                        const type = variation[0].trim()
-                          .toLowerCase()
-                          .replace(/[áâãà]/g, 'a')
-                          .replace(/[éê]/g, 'e')
-                          .replace(/[íî]/g, 'i')
-                          .replace(/[óôõ]/g, 'o')
-                          .replace(/[ú]/g, 'u')
-                          .replace(/[ç]/g, 'c')
-                          .replace(/-/g, '')
-                          .replace(/\s/g, '-')
-                          .replace(/[^0-9a-z-]/g, '')
-
-                        const text = variation[1].trim()
-                        let value
-                        if (type === 'colors') {
-                          switch (text.toLowerCase()) {
-                            case 'preto':
-                              value = '#000000'
-                              break
-                            case 'vermelho':
-                              value = '#ff0000'
-                              break
-                            case 'azul':
-                              value = '#0000ff'
-                              break
-                            case 'branco':
-                              value = '#ffffff'
-                              break
-                            case 'roxo':
-                              value = '#800080'
-                              break
-                            case 'cinza':
-                              value = '#808080'
-                              break
-                            case 'amarelo':
-                              value = '#ffff00'
-                              break
-                            case 'rosa':
-                              value = '#ff00ff'
-                              break
-                          }
-                        } else {
-                          value = text.toLowerCase()
-                        }
-                        specifications[type] = [{ text, value }]
-                      }
-                    }
-
-                    const trimName = pVariacao.nome.replace(/;/g, ' / ')
-                    const newVariation = {
-                      name: `${produto.descricao} / ${trimName}`,
-                      sku: pVariacao.codigo,
-                      quantity: pVariacao.estoqueAtual,
-                      specifications
-                    }
-
-                    appSdk
-                      .apiRequest(storeId, resource, 'POST', newVariation)
-                      .catch(e => {
-                        logger.error(`Variations Erro: ${data._id} | ${storeId} | Erro: ${e}`)
-                      })
-                  })
-                }
-                // call next product
-                return nextProduct()
-              })
-
-              .catch(e => {
-                //console.log(e)
-                const err = new Error('~<> Manual product synchronization failed')
-                err.error = e.message
-                err.storeId = storeId
-                if (e.response && e.response.data) {
-                  err.data = JSON.stringify(e.response.data)
-                }
-                err.product_sku = body[current]
-                logger.error('~<> ProductSyncFailed', JSON.stringify(err, undefined, 4))
-                nextProduct()
-              })
-          }
-        }
-
-        sync()
-        return res.end()
-      })
-
-      .catch(err => {
-        // logger.error(err)
-        // request to Store API with error response
-        // return error status code
-        res.status(500)
-        const { message } = err
-        res.send({
-          message
-        })
-      })
-  }
+    })
 }
